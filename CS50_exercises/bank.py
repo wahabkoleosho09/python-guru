fde71d058d24943a82965db2e84869ae20ec11a8
WAHAB KOLEOSHO <koleoshowahab84@gmail.com>
2026-09-20 15:15:08 +0100

    Added comment

--- CS50_exercises/bank.py
+++ CS50_exercises/bank.py
@@ -1,3 +1,8 @@
+"""
+ A program that prompts the user for a greeting. If the greeting starts with “hello”, output $0. 
+ If the greeting starts with an “h” (but not “hello”), output $20. 
+ Otherwise, output $100
+"""
 user = input("Greetings: ").strip().lower()
 
 if user.startswith("hello") or "hello" in user:
