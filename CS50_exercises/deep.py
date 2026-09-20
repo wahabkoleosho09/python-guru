270de223f0d0080a7b2747f60cce29c93228452d
WAHAB KOLEOSHO <koleoshowahab84@gmail.com>
2026-09-20 15:17:19 +0100

    Added comment

--- CS50_exercises/deep.py
+++ CS50_exercises/deep.py
@@ -1,3 +1,9 @@
+"""
+a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, 
+outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two.
+Otherwise output No
+"""
+
 user = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
 if user == "forty-two" or user == "forty two" or user == "42":
     print("Yes")
