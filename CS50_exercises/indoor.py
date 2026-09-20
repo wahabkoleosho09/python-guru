53dd994cce25913215615879af5220fc0d10081c
WAHAB KOLEOSHO <koleoshowahab84@gmail.com>
2026-09-20 15:24:06 +0100

    Added comment
    Update the program to prompt for user input and output it in lowercase.

--- CS50_exercises/indoor.py
+++ CS50_exercises/indoor.py
@@ -1,2 +1,8 @@
+"""
+a program in Python that prompts the user for input and then outputs that same input in lowercase.
+Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, 
+to prompt the user explicitly, as by passing a str of your own as an argument to input.
+"""
+
 user_message = input("Enter a message: ").strip().lower()
 print(user_message)
