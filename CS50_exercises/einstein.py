eaf1b3c6e0493d9aded5c34a85f3cb5fe538e913
WAHAB KOLEOSHO <koleoshowahab84@gmail.com>
2026-09-20 15:18:44 +0100

    Added comment
    Added a docstring to describe the program's functionality.

--- CS50_exercises/einstein.py
+++ CS50_exercises/einstein.py
@@ -1,3 +1,9 @@
+"""
+a program in Python that prompts the user for mass as an integer (in kilograms) and 
+then outputs the equivalent number of Joules as an integer. 
+Assume that the user will input an integer.
+"""
+
 # E = mc^2
 
 c = 300000000  # speed of light in m/s
