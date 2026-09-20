81900c9f4e0465778c9769bda511acee229b211d
WAHAB KOLEOSHO <koleoshowahab84@gmail.com>
2026-09-20 15:25:50 +0100

    Added comment
    Add a docstring explaining the program's functionality.

--- CS50_exercises/interpreter.py
+++ CS50_exercises/interpreter.py
@@ -1,3 +1,15 @@
+"""
+a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a 
+floating-point value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, 
+with one space between x and y and one space between y and z, wherein:
+
+x is an integer
+y is +, -, *, or /
+z is an integer
+For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, 
+if y is /, then z will not be 0.
+"""
+
 Expressions = input("Expressions: ").strip()
 a,operator,b = Expressions.split(" ")
 a = int(a)
